ac9af64203f78b734fa64c911378f0199ab6443f
Arun Prakash <arun.prakash.456789@gmail.com>
2020-08-17 21:42:03 +0530

    cleanup dashboard

--- Views/HomeView.php
+++ Views/HomeView.php
@@ -32,23 +32,6 @@ $_SESSION['PageTitle'] = "Home"
 </header>
 
 <body>
-
-    <input type="checkbox" id="check">
-    <div class="mobile_nav">
-        <div class="nav_bar">
-            <img src="" class="mobile_profile_image" alt="place_holder">
-            <i class="fa fa-bars nav_btn"></i>
-        </div>
-        <div class="mobile_nav_items">
-            <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
-            <a href="#"><i class="fas fa-cogs"></i><span>Components</span></a>
-            <a href="#"><i class="fas fa-table"></i><span>Tables</span></a>
-            <a href="#"><i class="fas fa-th"></i><span>Forms</span></a>
-            <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
-            <a href="#"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
-        </div>
-    </div>
-
     <div class="sidebar">
         <div class="profile_info">
             <img src="" class="profile_image" alt="place_holder">
@@ -61,7 +44,6 @@ $_SESSION['PageTitle'] = "Home"
         <a href="#" class="sidebar-item"><i class="fas fa-info-circle"></i><span>About</span></a>
         <a href="#" class="sidebar-item"><i class="fas fa-sliders-h"></i><span>Settings</span></a>
     </div>
-    <!--sidebar end-->
 
     <script type="text/javascript">
         $(document).ready(function() {
