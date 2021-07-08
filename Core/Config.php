ee9d70608245b5bcb0bc5217c485cc71ea03b9cb
Arun Prakash <arun.prakash.456789@gmail.com>
2021-07-08 19:52:09 +0530

    Update Config.php

--- Core/Config.php
+++ Core/Config.php
@@ -4,7 +4,7 @@ class Config
 {
     const HOST = "localhost",
         DB_USER_NAME = "arun",
-        DB_USER_PASSWORD = "miniprakash",
+        DB_USER_PASSWORD = "",
         BASE_URL = "http://localhost/zHost/",
         DB_NAME = "zhost",
         ADMIN_TABLE_NAME = "admin",
